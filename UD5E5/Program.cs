8d8e7921b7678022038fbdccc2eb64779999fcbc
Martina <martinaescorial9@gmail.com>
2021-06-21 17:53:26 +0200

    Add project files.

---
+++ UD5E5/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace UD5E5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String texto = "Introduce un numero";
+            int numero = Convert.ToInt32(texto);
+            String binario = decimalBinario(numero);
+            Console.WriteLine("El numero " + numero + " en binario es " + binario);
+        }
+        public static String decimalBinario(int numero)
+        {
+            String binario = "";
+            String digito;
+            for (int i = numero; i > 0; i /= 2)
+            {
+                if (i % 2 == 1)
+                {
+                    digito = "1";
+                }
+                else
+                {
+                    digito = "0";
+                }
+                
+                binario = digito + binario;
+            }
+            return binario;
+            
+        }
+    }
+}
